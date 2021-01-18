2e6903aad8bd93d54deb5e37c56652385328d550
Don Mosites <don.mosites@fluidity.io>
2021-01-18 15:41:57 +0100

    Prettify recent changes

--- src/state/trades/selectors.ts
+++ src/state/trades/selectors.ts
@@ -80,111 +80,102 @@ const makeGetTradesByQuery = createSelector(
   },
 );
 
-const makeGetTradeVolumeByDate = createSelector(
-  makeGetTradesForDate,
-  getTradesForDate => (query: TradeQuery) => {
-    const tradesByDay = {};
+const makeGetTradeVolumeByDate = createSelector(makeGetTradesForDate, getTradesForDate => (query: TradeQuery) => {
+  const tradesByDay = {};
 
-    const filteredTrades = getTradesForDate(query.days);
+  const filteredTrades = getTradesForDate(query.days);
 
-    if (!filteredTrades || !filteredTrades.length) return [];
+  if (!filteredTrades || !filteredTrades.length) return [];
 
-    for (let i = 0; i <= query.days; i++) {
-      const formattedDate = moment()
-        .subtract(i, 'd')
-        .format('MMM D, YYYY');
-      tradesByDay[formattedDate] = 0;
-    }
+  for (let i = 0; i <= query.days; i++) {
+    const formattedDate = moment()
+      .subtract(i, 'd')
+      .format('MMM D, YYYY');
+    tradesByDay[formattedDate] = 0;
+  }
 
-    // Pre-populate tradesByDay
-    filteredTrades
-      .filter((trade: SwapEvent) => {
-        if (!query.tokens || !query.tokens.length) return true;
-        return query.tokens.indexOf(trade.makerToken) !== -1 || query.tokens.indexOf(trade.takerToken) !== -1;
-      })
-      .forEach((trade: SwapEvent) => {
-        if (trade.timestamp) {
-          const timestampDate = moment.unix(trade.timestamp);
-          const formattedDate = timestampDate.format('MMM D, YYYY');
-          tradesByDay[formattedDate] += trade.ethAmount || 0;
-        }
-      });
-    return Object.keys(tradesByDay)
-      .map(date => ({ date, volume: tradesByDay[date] }))
-      .reverse();
-  },
-);
-
-const makeGetTradeVolumeByToken = createSelector(
-  makeGetTradesForDate,
-  getTradesForDate => (days: number) => {
-    const tradesByToken = {};
-    const filteredTrades = getTradesForDate(days);
-
-    filteredTrades.forEach((trade: SwapEvent) => {
+  // Pre-populate tradesByDay
+  filteredTrades
+    .filter((trade: SwapEvent) => {
+      if (!query.tokens || !query.tokens.length) return true;
+      return query.tokens.indexOf(trade.makerToken) !== -1 || query.tokens.indexOf(trade.takerToken) !== -1;
+    })
+    .forEach((trade: SwapEvent) => {
       if (trade.timestamp) {
-        if (tradesByToken[trade.makerToken]) {
-          tradesByToken[trade.makerToken] += trade.ethAmount || 0;
+        const timestampDate = moment.unix(trade.timestamp);
+        const formattedDate = timestampDate.format('MMM D, YYYY');
+        tradesByDay[formattedDate] += trade.ethAmount || 0;
+      }
+    });
+  return Object.keys(tradesByDay)
+    .map(date => ({ date, volume: tradesByDay[date] }))
+    .reverse();
+});
+
+const makeGetTradeVolumeByToken = createSelector(makeGetTradesForDate, getTradesForDate => (days: number) => {
+  const tradesByToken = {};
+  const filteredTrades = getTradesForDate(days);
+
+  filteredTrades.forEach((trade: SwapEvent) => {
+    if (trade.timestamp) {
+      if (tradesByToken[trade.makerToken]) {
+        tradesByToken[trade.makerToken] += trade.ethAmount || 0;
+      } else {
+        tradesByToken[trade.makerToken] = trade.ethAmount || 0;
+      }
+
+      if (tradesByToken[trade.takerToken]) {
+        tradesByToken[trade.takerToken] += trade.ethAmount || 0;
+      } else {
+        tradesByToken[trade.takerToken] = trade.ethAmount || 0;
+      }
+    }
+  });
+
+  return tradesByToken;
+});
+
+const makeGetTradeVolumeByTrader = createSelector(makeGetTradesForDate, getTradesForDate => (query: TradeQuery) => {
+  const tradeVolumeByTrader = {};
+  const filteredTrades = getTradesForDate(query.days);
+
+  filteredTrades
+    .filter((trade: SwapEvent) => {
+      if (query.tokens && query.tokens.length) {
+        return query.tokens.indexOf(trade.takerToken) !== -1 || query.tokens.indexOf(trade.makerToken) !== -1;
+      }
+      return true;
+    })
+    .forEach((trade: SwapEvent) => {
+      if (trade.timestamp) {
+        if (tradeVolumeByTrader[trade.makerAddress]) {
+          tradeVolumeByTrader[trade.makerAddress] = {
+            totalTrades: tradeVolumeByTrader[trade.makerAddress].totalTrades + 1,
+            volume: tradeVolumeByTrader[trade.makerAddress].volume + trade.ethAmount || 0,
+          };
         } else {
-          tradesByToken[trade.makerToken] = trade.ethAmount || 0;
+          tradeVolumeByTrader[trade.makerAddress] = {
+            totalTrades: 1,
+            volume: trade.ethAmount || 0,
+          };
         }
 
-        if (tradesByToken[trade.takerToken]) {
-          tradesByToken[trade.takerToken] += trade.ethAmount || 0;
+        if (tradeVolumeByTrader[trade.takerAddress]) {
+          tradeVolumeByTrader[trade.takerAddress] = {
+            totalTrades: tradeVolumeByTrader[trade.takerAddress].totalTrades + 1,
+            volume: tradeVolumeByTrader[trade.takerAddress].volume + trade.ethAmount || 0,
+          };
         } else {
-          tradesByToken[trade.takerToken] = trade.ethAmount || 0;
+          tradeVolumeByTrader[trade.takerAddress] = {
+            totalTrades: 1,
+            volume: trade.ethAmount || 0,
+          };
         }
       }
     });
 
-    return tradesByToken;
-  },
-);
-
-const makeGetTradeVolumeByTrader = createSelector(
-  makeGetTradesForDate,
-  getTradesForDate => (query: TradeQuery) => {
-    const tradeVolumeByTrader = {};
-    const filteredTrades = getTradesForDate(query.days);
-
-    filteredTrades
-      .filter((trade: SwapEvent) => {
-        if (query.tokens && query.tokens.length) {
-          return query.tokens.indexOf(trade.takerToken) !== -1 || query.tokens.indexOf(trade.makerToken) !== -1;
-        }
-        return true;
-      })
-      .forEach((trade: SwapEvent) => {
-        if (trade.timestamp) {
-          if (tradeVolumeByTrader[trade.makerAddress]) {
-            tradeVolumeByTrader[trade.makerAddress] = {
-              totalTrades: tradeVolumeByTrader[trade.makerAddress].totalTrades + 1,
-              volume: tradeVolumeByTrader[trade.makerAddress].volume + trade.ethAmount || 0,
-            };
-          } else {
-            tradeVolumeByTrader[trade.makerAddress] = {
-              totalTrades: 1,
-              volume: trade.ethAmount || 0,
-            };
-          }
-
-          if (tradeVolumeByTrader[trade.takerAddress]) {
-            tradeVolumeByTrader[trade.takerAddress] = {
-              totalTrades: tradeVolumeByTrader[trade.takerAddress].totalTrades + 1,
-              volume: tradeVolumeByTrader[trade.takerAddress].volume + trade.ethAmount || 0,
-            };
-          } else {
-            tradeVolumeByTrader[trade.takerAddress] = {
-              totalTrades: 1,
-              volume: trade.ethAmount || 0,
-            };
-          }
-        }
-      });
-
-    return tradeVolumeByTrader;
-  },
-);
+  return tradeVolumeByTrader;
+});
 
 const makeGetVolumeDistributionBySource = createSelector(
   makeGetTradesByQuery,
