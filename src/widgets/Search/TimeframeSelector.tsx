13806a137f27e7e777a03d75e2c6fa3e0e69c7fd
Don Mosites <don.mosites@fluidity.io>
2021-02-07 14:43:54 +0100

    Reduce to 7-day

--- src/widgets/Search/TimeframeSelector.tsx
+++ src/widgets/Search/TimeframeSelector.tsx
@@ -4,7 +4,6 @@ import { FormattedMessage } from 'react-intl';
 import { QueryContext } from '../../app/context/QueryContext';
 import Flex from '../../components/Flex';
 import MediaQuery from '../../components/MediaQuery';
-import { HorizontalSpacer } from '../../components/Spacer';
 import { TimeframeOption } from './styles';
 
 export default function TimeframeSelector() {
@@ -16,26 +15,10 @@ export default function TimeframeSelector() {
         <TimeframeOption selected={timeframe === 7} onClick={() => setTimeframe(7)}>
           <FormattedMessage defaultMessage="7d" />
         </TimeframeOption>
-        <HorizontalSpacer units={2} />
-        <TimeframeOption selected={timeframe === 14} onClick={() => setTimeframe(14)}>
-          <FormattedMessage defaultMessage="2w" />
-        </TimeframeOption>
-        <HorizontalSpacer units={2} />
-        <TimeframeOption selected={timeframe === 30} onClick={() => setTimeframe(30)}>
-          <FormattedMessage defaultMessage="1m" />
-        </TimeframeOption>
       </MediaQuery>
       <MediaQuery size="md-up">
         <TimeframeOption selected={timeframe === 7} onClick={() => setTimeframe(7)}>
           <FormattedMessage defaultMessage="7 Days" />
-        </TimeframeOption>
-        <HorizontalSpacer units={3} />
-        <TimeframeOption selected={timeframe === 14} onClick={() => setTimeframe(14)}>
-          <FormattedMessage defaultMessage="2 Weeks" />
-        </TimeframeOption>
-        <HorizontalSpacer units={3} />
-        <TimeframeOption selected={timeframe === 30} onClick={() => setTimeframe(30)}>
-          <FormattedMessage defaultMessage="1 Month" />
         </TimeframeOption>
       </MediaQuery>
     </Flex>
